da50b40b99f286bf1b2a9c5da69bbbf4d0f6400c
Afaaq <afaaqmajeed123@gmail.com>
2022-08-08 10:33:00 +0530

    added

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header__logo'>
+                <img src="https://i0.wp.com/www.dafontfree.co/wp-content/uploads/2021/11/Amazon-Logo-Font-1-scaled.jpg?fit=2560%2C1578&ssl=1" alt="amazon logo" />
+            </div>
+
+            <div className='header__search'>
+                <input type="text" className="header__searchinput" />
+                <SearchIcon className="header__searchicon" />
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className='header__option1'>Hello</span>
+                    <span className='header__option2'>SignIn</span>
+                </div>
+
+                <div className="header__option">
+                    <span className='header__option1'>Returns</span>
+                    <span className='header__option2'> & Orders</span>
+                </div>
+
+                <div className="header__option">
+                    <div>
+                        <span className='header__option1' ><ShoppingCartIcon /></span>
+                        <span className='header__option2'>0</span>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
